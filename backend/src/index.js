30bd85c00c3755e94cacb6db2dd82388ce28f6e1
edmarques11 <edmarquessantosdelima@gmail.com>
2021-02-17 16:11:48 -0300

    finalizaçao da primeira parte do curso

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,22 +2,33 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/projects", (request, response) => {
+  const { title, owner } = request.query;
+  console.log(title, owner);
   return response.json(["Projeto 1", "Projeto 2", "Projeto 3", "Projeto 4"]);
 });
 
 app.post("/projects", (request, response) => {
+  const body = request.body;
+
+  console.log(body);
   return response.json(["Projeto 5"]);
 });
 
-app.put("/projects", (request, response) => {
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(`project ${id} is changed`);
   return response.json({
     before: "Projeto 5",
     after: "Projeto 5...",
   });
 });
 
-app.delete("/projects", (request, response) => {
+app.delete("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(`project ${id} is deleted`);
   return response.json({
     deleted: ["Projeto 1", "Projeto 2", "Projeto 3", "Projeto 4"],
   });
